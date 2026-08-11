64ef35ea9a9f5cd937ea22533e213f7142edb3c9
Faiz-Super <faizmhmad15@gmail.com>
2026-08-11 23:06:56 +0700

    update lagi size API

--- app/api/[storeId]/sizes/route.ts
+++ app/api/[storeId]/sizes/route.ts
@@ -44,15 +44,19 @@ export async function GET(
   { params }: { params: { storeId: string } }
 ) {
   try {
-    if (!params.storeId) return new NextResponse("Store id is required", { status: 400 });
+    const { storeId } = await params;
+
+    if (!storeId) return new NextResponse("Store id is required", { status: 400 });
 
     const sizes = await db.size.findMany({
-      where: { storeId: params.storeId }
+      where: { 
+        storeId: storeId 
+      }
     });
 
     return NextResponse.json(sizes);
   } catch (error) {
-    console.log('[SIZES_GET]', error);
-    return new NextResponse("Internal error", { status: 500 });
+      console.log('[SIZES_GET]', error);
+      return new NextResponse("Internal error", { status: 500 });
   }
 }
